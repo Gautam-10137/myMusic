f9cdcf2538b9f36c431e0d8053b6322b056f047f
Gautam-10137 <pahwagautam786@gmail.com>
2023-09-26 18:23:41 +0530

    project started

---
+++ src/Player.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useSound from "use-sound"
+const Player = () => {
+  return (
+    <div className='component'>
+        <h1>My-Music</h1>
+        <h2>Playing Now</h2>
+      
+    </div>
+  )
+}
+
+export default Player
